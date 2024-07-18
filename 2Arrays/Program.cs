b3601936d955d68285a237e6ae4188306ba33d72
Chubi1ce <Cae_2003@mail.ru>
2024-07-18 11:18:39 +0700

    Add 2 tasks

---
+++ 2Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    /*
+    Напишите приложение для одновременного выполнения двух задач в потоках.
+    Нужно подсчитать сумму элементов каждого из массивов а потом сложить
+    эти суммы полученные после выполнения каждого из потоков и вывести результат на экран
+    */
+    static int[] arr1 = { 1, 2, 3 };
+    static int[] arr2 = { 4, 5, 6 };
+
+    static int summ1, summ2;
+    public static void getSumm1()
+    {
+        summ1 = 0;
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            summ1 += arr1[i];
+        }
+    }
+    public static void getSumm2()
+    {
+        summ2 = 0;
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            summ2 += arr2[i];
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Thread tr1 = new Thread(getSumm1);
+        tr1.Start();
+        tr1.Join();
+        Console.WriteLine(summ1);
+
+        Thread tr2 = new Thread(getSumm2);
+        tr2.Start();
+        tr2.Join();
+        Console.WriteLine(summ2);
+
+        Console.WriteLine(summ1 + summ2);
+    }
+}
---
+++ Ping/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+/*
+Напишите многопоточное приложение которое определяет
+все IP-адресаинтернет-ресурса
+и определяет до которого их них лучше Ping
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string resource = "yandex.ru";
+        var IpAdresses = Dns.GetHostAddresses(resource, AddressFamily.InterNetwork);
+        Dictionary<IPAddress, long> pings = new Dictionary<IPAddress, long>(); ;
+        List<Thread> threads = new List<Thread>();
+
+        foreach (var address in IpAdresses)
+        {
+            Console.WriteLine(address);
+        }
+
+        foreach (var address in IpAdresses)
+        {
+            Thread tr = new Thread(() =>
+            {
+                Ping ping = new Ping();
+                PingReply pingReply = ping.Send(resource);
+
+                pings.Add(address, pingReply.RoundtripTime);
+            });
+
+            threads.Add(tr);
+            tr.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        long min = long.MaxValue;
+        
+        foreach (var ping in pings)
+        {
+            Console.WriteLine($"IP: {ping.Key}: Ping: {ping.Value}");
+            if (ping.Value < min)
+            {
+                min = ping.Value;
+            }
+        }
+        
+        Console.WriteLine($"Minimum Ping = {min}");
+
+
+    }
+}
